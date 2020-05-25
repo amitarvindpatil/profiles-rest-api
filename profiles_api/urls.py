4d5d1deeb293369a4623ab7bb4d385364f8584cb
amit <amitpatil04041993@gmail.com>
2020-05-25 20:40:41 +0530

    profile creation

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -6,6 +6,7 @@ from profiles_api import views
 """This is for viewset"""
 router = DefaultRouter()
 router.register('hello-viewset',views.HelloViewSet, basename='hello-viewset')
+router.register('profile', views.UserProfileViewSet)
 
 urlpatterns = [
     path('hello-view/',views.HelloApiView.as_view()),
--- profiles_api/views.py
+++ profiles_api/views.py
@@ -2,8 +2,12 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
+from rest_framework import filters
 
 from profiles_api import serializers
+from profiles_api import models
+from profiles_api import permissions
 
 class HelloApiView(APIView):
     """Test API View"""
@@ -84,3 +88,19 @@ class HelloViewSet(viewsets.ViewSet):
     def destroy(self,request,pk=None):
         """Handdle Removing object"""
         return Response({'http_method':'DELETE'})
+
+
+# class UserProfileViewSet(viewsets.ModelViewSet):
+#     """Handdle creating and updating profiles"""
+#     print("helo")
+#     serializer_class = serializers.UserProfileSerializer
+#     queryset = models.UserProfile.objects.all()
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """Handle creating and updating profiles"""
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields  = ('name','email',)
