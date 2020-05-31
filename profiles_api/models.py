c2092bdc13c77e2f1f3cd4940740f752cc2b180f
amit <amitpatil04041993@gmail.com>
2020-05-31 14:31:43 +0530

    permission,userstatus and autharizations

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin
 from django.contrib.auth.models import BaseUserManager
+from django.conf import settings
 
 # Create your models here.
 class UserProfileManager(BaseUserManager):
@@ -30,6 +31,8 @@ class UserProfileManager(BaseUserManager):
 
 class UserProfile(AbstractBaseUser,PermissionsMixin):
     """ DataBase model for user in a system  """
+
+
     email = models.EmailField(max_length=255,unique=True)
     name  = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
@@ -51,3 +54,15 @@ class UserProfile(AbstractBaseUser,PermissionsMixin):
     def __str__(self):
         """ Retrive String representation of user"""
         return self.email
+
+class ProfileFeedItem(models.Model):
+    """Profile Status Update"""
+    user_profile = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+    status_text = models.CharField(max_length=255)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.status_text
